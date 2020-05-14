e69cf68e28ccc555cb3ce20b2bdaee2dc0dbbd2b
Carleone <carleonepsouza@gmail.com>
2020-05-14 14:26:51 -0300

    Fix the footer in home page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,11 @@
 export const environment = {
-  production: false
+    apiKey: 'AIzaSyBCeA2oiwvOsZaE5XMYqAnG6n63riXGGx4',
+    authDomain: 'periodic-app.firebaseapp.com',
+    databaseURL: 'https://periodic-app.firebaseio.com',
+    projectId: 'periodic-app',
+    storageBucket: 'periodic-app.appspot.com',
+    messagingSenderId: '902819840447',
+    appId: '1:902819840447:web:2f868d5021b3c723d3a10a',
+    measurementId: 'G-R81ZM2DDJR'
 };
 
